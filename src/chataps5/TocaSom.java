aa24a8f1a582d84b376c641fd8aa3e0145dae96c
viniciussilverio <33009217+viniciussilverio@users.noreply.github.com>
2018-05-27 11:26:26 -0300

    Correção de erro para abrir som no JAR.

--- src/chataps5/TocaSom.java
+++ src/chataps5/TocaSom.java
@@ -1,11 +1,14 @@
 package chataps5;
 
-import java.applet.Applet;
-import java.applet.AudioClip;
-import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class TocaSom {
     public static void Tocar(){
@@ -14,34 +17,30 @@ public class TocaSom {
     
     public static void Tocar(int som){
         try {
-            File toque;
-            File file1 =  new File("alert.wav");
-            File file2, file3;
-            if(!file1.exists()) {
-                file1 =  new File("..\\alert.wav");
-                file2 =  new File("..\\alert.wav");
-                file3 =  new File("..\\alert.wav");
-            }else{
-                file2 =  new File("alert.wav");
-                file3 =  new File("alert.wav");
-            }
+            Clip clip = AudioSystem.getClip();
+
+            URL file1, file2, file3;
+            file1 =  TocaSom.class.getResource("/media/alert.wav");
+            file2 =  TocaSom.class.getResource("/media/alert.wav");
+            file3 =  TocaSom.class.getResource("/media/alert.wav");
             switch(som){
                 case 1:
-                    toque = file1;
+                    clip.open(AudioSystem.getAudioInputStream(file1));
                     break;
                 case 2:
-                    toque = file2;
+                    clip.open(AudioSystem.getAudioInputStream(file2));
                     break;
                 case 3:
-                    toque = file3;
+                    clip.open(AudioSystem.getAudioInputStream(file3));
                     break;
                 default:
-                    toque = file1;
+                    clip.open(AudioSystem.getAudioInputStream(file1));
                 break;
             }
-            AudioClip clip = Applet.newAudioClip(toque.toURL());
-            clip.play();
+            clip.start();
         } catch (MalformedURLException ex) {
+            Logger.getLogger(TocaSom.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException ex) {
             Logger.getLogger(TocaSom.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
